3579e870f6ab174c5fefcbd2289555bb4ae3edc6
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-13 00:33:30 +0300

    ALL DONE

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,8 @@ fun main() {
     greetSeaCreatures()
     println(findPearl(6))
     println(divideTreasure(1000.0, 5.0))
+    println(calculateDepth(1500, 800))
+    println(diveMessage("deep sea"))
 
 }
 
@@ -11,3 +13,12 @@ val greetSeaCreatures = {println("Hello, Deep Sea Adventurer!")}
 val findPearl = {number: Int ->  number * number }
 
 val divideTreasure = {totalTreasure: Double, numberExplorers: Double -> totalTreasure/numberExplorers}
+
+val calculateDepth: (Int, Int) -> Int = {x, y -> x-y}
+
+fun String.loud(): String{
+    return this.uppercase() + "!"
+}
+val diveMessage: (String) -> String = { message -> message.loud() }
+
+
