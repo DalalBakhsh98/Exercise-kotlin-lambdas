124c86411fde031940381da4a995c5db4b05aba8
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-12 19:02:12 +0300

    Finished till part3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+
+    greetSeaCreatures()
+    println(findPearl(6))
+    println(divideTreasure(1000.0, 5.0))
+
 }
+
+val greetSeaCreatures = {println("Hello, Deep Sea Adventurer!")}
+
+val findPearl = {number: Int ->  number * number }
+
+val divideTreasure = {totalTreasure: Double, numberExplorers: Double -> totalTreasure/numberExplorers}
